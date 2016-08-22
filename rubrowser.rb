7476c6d4ff56bc098a2fd61d97fcc900b13ca4f7
Emad Elsaid <emad.elsaid@wimdu.com>
2016-08-22 22:28:33 +0200

    a working setup for my side project

--- rubrowser.rb
+++ rubrowser.rb
@@ -2,57 +2,131 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
-FILE_SIZE_LIMIT = 2 * 1024 * 1024
 
-vertices = Set.new
-edges = Set.new
+class Tree
+  attr_reader :value, :children
 
-def parse_block(node)
-  return unless node.is_a?(Parser::AST::Node)
+  def initialize(value)
+    @value = value
+    @children = Set.new
+  end
 
-  case node.type
-  when :module
-    parse_module(node)
-  when :class
-    parse_class(node)
-  else
-    node.children.each { |n| parse_block(n) }
+  def add_child(child_name_path = [])
+    return if child_name_path.empty?
+    child = get_or_create_child(child_name_path[0])
+    children.add(child)
+    child.add_child(child_name_path[1..-1])
+  end
+
+  def get_or_create_child(child_name)
+    children.find { |child| child_name == child.value } || Tree.new(child_name)
+  end
+
+  def eq?(other)
+    other.value == value
   end
 end
 
-def parse_module(node)
-  name = const_to_array(node.children.first)
-  puts "MODULE #{name}"
-  node.children[1..-1].each { |n| parse_block(n) }
-end
+class FileParser
+  FILE_SIZE_LIMIT = 2 * 1024 * 1024
+  attr_reader :file, :definitions
 
-def parse_class(node)
-  name = const_to_array(node.children.first)
-  body = node.children[1..-1]
-  puts "CLASS #{name}"
-  body.each { |n| parse_block(n) }
-end
+  def initialize(file)
+    @file = file
+    @definitions = Set.new
+    parse
+  end
 
-def const_to_array(node)
-  return [] unless node.is_a?(Parser::AST::Node) && node.type == :const
-  const_to_array(node.children.first) + [node.children.last]
-end
+  private
 
-def parse_file(file)
-  if !File.symlink?(file) && File.file?(file) && File.size(file) <= FILE_SIZE_LIMIT
-    file_handle = File.open(file)
-    code = file_handle.read
-    file_handle.close
-    ast = Parser::CurrentRuby.parse(code)
-    parse_block ast
-  elsif File.directory?(file)
-    files = Dir.glob(File.join(file, '**', '*.rb'))
-    Parallel.each(files) do |file_path|
-      parse_file(file_path)
+  attr_writer :definitions
+
+  def parse_block(node, parents = [])
+    return [] unless node.is_a?(Parser::AST::Node)
+
+    case node.type
+    when :module
+      parse_module(node, parents)
+    when :class
+      parse_class(node, parents)
+    else
+      node.children.map { |n| parse_block(n, parents) }.reduce(:+) || []
     end
   end
+
+  def parse_module(node, parents = [])
+    name = resolve_const_path(node.children.first, parents)
+    node.children[1..-1].map { |n| parse_block(n, name) }.reduce(:+).push(name)
+  end
+
+  def parse_class(node, parents = [])
+    name = resolve_const_path(node.children.first, parents)
+    body = node.children[1..-1]
+    body.map { |n| parse_block(n, name) }.reduce(:+).push(name)
+  end
+
+  def resolve_const_path(node, parents = [])
+    return parents unless node.is_a?(Parser::AST::Node) && node.type == :const
+    resolve_const_path(node.children.first, parents) + [node.children.last]
+  end
+
+  def parse
+    if file_valid?(file)
+      File.open(file) do |f|
+        code = f.read
+        ast = Parser::CurrentRuby.parse(code)
+        self.definitions = parse_block(ast)
+      end
+    else
+      []
+    end
+  end
+
+  def file_valid?(file)
+    !File.symlink?(file) && File.file?(file) && File.size(file) <= FILE_SIZE_LIMIT
+  end
 end
 
-ARGV.each do |file|
-  parse_file(file)
+class DirectoryParser
+  attr_reader :directory
+
+  def initialize(directory)
+    @directory = directory
+    @parsers = []
+    parse
+  end
+
+  def definitions
+    parsers.map(&:definitions) || []
+  end
+
+  private
+
+  attr_accessor :parsers
+
+  def parse
+    files = Dir.glob(File.join(directory, '**', '*.rb'))
+    self.parsers = Parallel.map(files) do |file|
+      if File.file?(file)
+        FileParser.new(file)
+      elsif File.directory?(file)
+        DirectoryParser.new(file)
+      end
+    end.compact
+  end
 end
+
+parsers = ARGV.map do |file|
+  if File.file?(file)
+    FileParser.new(file)
+  elsif File.directory?(file)
+    DirectoryParser.new(file)
+  end
+end
+
+names = parsers.map(&:definitions).reduce(:+)
+constants_tree = Tree.new(nil)
+names.each { |name| constants_tree.add_child(name) }
+
+require 'yaml'
+puts constants_tree.to_yaml
